dd60d89be198e238876b4413036ecd3faf91fb5f
Alexander <Zadir7@mail.ru>
2021-02-22 13:52:43 +0700

    Iteraton 2
    Added player rotation
    Added bullet prefab

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,21 +4,55 @@ namespace Game
 {
     public class PlayerController : MonoBehaviour
     {
-        public float speed;
+        [SerializeField] private float _speed;
+        [SerializeField] private float _rotationSpeed;
 
         private Vector3 _direction;
+        private float _rotationValue;
+        private bool _isFiring = false;
+        private Transform _gun;
+
+        private void Awake()
+        {
+            _gun = GetComponentInChildren<Transform>();
+        }
 
         private void Update()
         {
-            _direction.x = Input.GetAxis("Horizontal");
+            _rotationValue = Input.GetAxis("Horizontal");
             _direction.z = Input.GetAxis("Vertical");
+
+            if (Input.GetAxis("Fire1") > 0)
+            {
+                _isFiring = true;
+            }
         }
 
         private void FixedUpdate()
         {
-            var movementTarget = _direction * speed * Time.fixedDeltaTime;
-            transform.Translate(movementTarget);
+            Move();
+            Rotate();
+
+            if (_isFiring)
+            {
+                Fire();
+                _isFiring = false;
+            }
         }
 
+        private void Move()
+        {
+            Vector3 movementTarget = _direction * _speed * Time.fixedDeltaTime;
+            transform.Translate(movementTarget);
+        }
+        private void Rotate()
+        {
+            transform.Rotate(new Vector3(0, _rotationValue * _rotationSpeed * Time.fixedDeltaTime, 0));
+        }
+
+        private void Fire()
+        {
+
+        }
     }
 }
